11683f22419bc891aa7bdcd8ee10c07d15b9ee62
fence <fence@desu-mail.moe>
2020-07-20 15:41:21 +0200

    clean up example

--- examples/basic/src/main.c
+++ examples/basic/src/main.c
@@ -1,12 +1,8 @@
-//#define _SFR_ASM_COMPAT 1
 #include <owoLED.h>
 #include <util/delay.h>
 #include <avr/io.h> 
-//#include <avr/sfr_defs.h> 
 
-#define PIXELS 60  // Number of pixels in the string
-
-//volatile uint8_t *port = (uint8_t *) _SFR_IO8(0x05);
+#define PIXELS 60  // Number of pixels in the strip
 
 int main (void) {
     OwOLedAddress addr = owoled_init(&PORTB, &DDRB, 1);
